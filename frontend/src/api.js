52116b6c83459646c8a0a798d40996203cc8d95f
gnaneshwar <gnaeshwaryadav@gmail.com>
2026-03-09 23:05:25 +0530

    Fix env config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import dayjs from "dayjs";
 
-const baseURL = "http://127.0.0.1:8000/api";
+const baseURL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
 
 const api = axios.create({
   baseURL,
