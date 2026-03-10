6e0331fb073ce28e39aa359e138771f3bd3193d7
gnaneshwar <gnaeshwaryadav@gmail.com>
2026-03-10 21:45:20 +0530

    Fix CORS - remove CORS_ALLOWED_ORIGINS, use CORS_ALLOW_ALL_ORIGINS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -136,10 +136,7 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': False,
 }
 
-CORS_ALLOWED_ORIGINS = os.environ.get(
-    'CORS_ALLOWED_ORIGINS',
-    'http://localhost:5173,http://127.0.0.1:5173'
-).split(',')
+
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
